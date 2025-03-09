2999dd966fdb52fb39da2fa5d4883d6450f35aaa
DESKTOP <ziwoo73@gmail.com>
2025-03-09 18:41:47 +0900

    fix slider

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
   const slides = document.querySelectorAll('.slide');
   let currentSlide = 0;
 
-  // 모든 슬라이드를 숨김
+  // 모든 슬라이드를 숨기고, 첫 번째 슬라이드를 표시
   slides.forEach(slide => slide.style.display = 'none');
-
-  // 첫 번째 슬라이드 표시
   slides[currentSlide].style.display = 'block';
 
   // 3초마다 슬라이드 전환
